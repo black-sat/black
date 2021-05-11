379a57b0e67bbf019439046cbc36c61408261236
Nicola Gigante <nicola.gigante@gmail.com>
2021-05-11 17:12:54 +0200

    Debug message removed

--- src/frontend/src/tracecheck.cpp
+++ src/frontend/src/tracecheck.cpp
@@ -245,7 +245,7 @@ namespace black::frontend
 
     memo.insert({{f,t}, result});
 
-    io::message("check(trace, {}, {}) = {}", to_string(f), t, result);
+    //io::message("check(trace, {}, {}) = {}", to_string(f), t, result);
     return result;
   }
 
