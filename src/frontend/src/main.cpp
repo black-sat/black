e4100df3fe7d37c46272d203aa14c70b7eaff02f
Nicola Gigante <nicola.gigante@gmail.com>
2021-05-12 09:25:45 +0200

    Added backtrace reporting on SIGSEGV

--- src/frontend/src/main.cpp
+++ src/frontend/src/main.cpp
@@ -28,8 +28,27 @@
 
 using namespace black::frontend;
 
+#include <csignal>
+#include <cstdio>
+#include <execinfo.h>
+#include <unistd.h>
+
+void handler(int sig) {
+  void *array[100];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 100);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+
 int main(int argc, char **argv)
 {
+  signal(SIGSEGV, handler);
   parse_command_line(argc, argv);
   
   if(cli::dimacs)
