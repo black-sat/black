0baf7ee8d4df51d4b777ec9546141fbef6314bbb
Nicola Gigante <nicola.gigante@gmail.com>
2021-08-13 20:04:18 +0200

    TDLLite formulas results

--- benchmarks/scripts/aalta/pre.sh
+++ benchmarks/scripts/aalta/pre.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -e "s/~/\!/g" -e "s/=>/->/g" -e "s/True/TRUE/g" -e "s/False/FALSE/g" < $1
+sed -e "s/~/\!/g" -e "s/=>/->/g" -e "s/True/TRUE/g" -e "s/False/FALSE/g" -e "s/wX/N/g" < $1
