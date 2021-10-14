a9bb0c05d57bcab664469724db38299f7de1b3ea
Nicola Gigante <nicola.gigante@gmail.com>
2021-10-14 16:54:31 +0200

    NuXmv preprocessing script update

--- benchmarks/scripts/nuXmv/pre.sh
+++ benchmarks/scripts/nuXmv/pre.sh
@@ -5,8 +5,10 @@ MODULE main
 VAR
 END
 
-cat $1 | grep -o '([a-zA-Z0-9]\{1,\})' | sort | uniq |
-  sed 's/(\(.*\))/   \1 : boolean;/'
+cat $1 | grep -o '[a-zA-Z0-9_]\{1,\}' | sort | uniq |
+  grep -vw "X" | grep -vw "F" | grep -vw "G" | grep -vw "U" | grep -vw "R" | 
+  grep -vw "Y" | grep -vw "P" | grep -vw "H" | grep -vw "S" | grep -vw "T" | 
+  sed 's/\(.*\)/   \1 : boolean;/'
 
 formula=$(cat $1 |
   sed 's/=>/->/g;s/~/!/g;s/True/TRUE/g;s/False/FALSE/g;s/ R / V /g')
