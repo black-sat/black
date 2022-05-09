4b545f9dce546092870b861602dd6d3c0f77fb76
Nicola Gigante <nicola.gigante@gmail.com>
2022-05-09 17:03:18 +0200

    AppVeyor artifact download and final touches in release upload script

--- release.sh
+++ release.sh
@@ -4,7 +4,15 @@ set -eu -o pipefail
 shopt -s failglob
 
 die() {
+  echo "$@"
   exit 1
+}
+
+dependencies() {
+  which wget || die "Missing wget"
+  which curl || die "Missing curl"
+  which jq   || die "Missing jq"
+  which gh   || die "Missing gh"
 }
 
 setup() {
@@ -57,6 +65,34 @@ build() {
      "$SRC_DIR/packages/black-sat-$VERSION-1.$(uname -m).$ext" 
 }
 
+appveyor() {
+  AV_API=https://ci.appveyor.com/api
+  AV_TOKEN=$(cat appveyor.token)
+  AV_USER=nicola-gigante
+  AV_REPO=black
+  AV_PROJECTS_URL=$AV_API/projects/$AV_USER/$AV_REPO
+
+  api() {
+    curl -sS -L --header "Content-type: application/json" --header "Authorization: Bearer $AV_TOKEN" "$@"
+  }
+
+  CURRENT_COMMIT=$(git rev-parse HEAD)
+  BUILD_COMMIT=$(api "$AV_PROJECTS_URL" | jq -r .build.commitId)
+  if [ "$CURRENT_COMMIT" != "$BUILD_COMMIT" ]; then
+    echo Latest Appveyor build is not for the current commit. Aborting...
+    die
+  fi
+  JOB_ID=$(api "$AV_PROJECTS_URL" | jq -r '.build.jobs[0].jobId')
+  
+  AV_ARTIFACTS_URL=$AV_API/buildjobs/$JOB_ID/artifacts
+  N_ARTIFACTS=$(api "$AV_ARTIFACTS_URL" | jq '. | length')
+  if [ "$N_ARTIFACTS" -eq 0 ]; then
+    echo No artifact available in Appveyor. Aborting...
+    die
+  fi
+  api "$AV_ARTIFACTS_URL"/black-win-x64.zip -o packages/black-$VERSION-win-x64.zip
+}
+
 release() {
   cat gh.token | gh auth login -p ssh --with-token
   temp=$(mktemp)
@@ -68,6 +104,7 @@ release() {
   gh release create --notes-file $temp -p -t v$VERSION --target master v$VERSION
   gh release upload v$VERSION packages/black-sat-$VERSION-1.x86_64.deb
   gh release upload v$VERSION packages/black-sat-$VERSION-1.x86_64.rpm
+  gh release upload v$VERSION packages/black-$VERSION-win-x64.zip
 }
 
 homebrew() {
@@ -105,12 +142,14 @@ END
 }
 
 main () {
+  dependencies
   setup
   images
   build ubuntu 
   build fedora
+  appveyor
   release
-  # homebrew
+  homebrew
 }
 
 main "$@"
