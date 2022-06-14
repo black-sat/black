76d0390625006bbef3a5b4651203cb7626220a97
Nicola Gigante <nicola.gigante@gmail.com>
2022-06-14 15:31:52 +0200

    Documentation comments for hierarchy.hpp

--- src/lib/include/black/new/internal/formula/hierarchy.hpp
+++ src/lib/include/black/new/internal/formula/hierarchy.hpp
@@ -22,7 +22,180 @@
 // SOFTWARE.
 
 //
-// Note: no include guards. This file is designed to be included many times.
+// This is the hierarchy definition file. Through a number of macros (that are
+// *not* defined here) we define the syntactic structure of the available
+// formulas, terms, etc... This file is thought to be included multiple times.
+// By defining each time in a different way the macros, one can create pretty
+// complex structures based on the hierarchy defined here. See `generation.hpp`
+// to see the actual code generation happening. Here, we comment the meaning of
+// these macros from a modeling perspective. Together they form a
+// domain-specific language to define ADT-like hierarchies of types.
+//
+// This file is structured in three parts:
+// - the first part defines all the macros as empty, *if* they are not defined.
+//   This means that when including this file, only the single macros needed for
+//   that specific inclusion have to be defined. Some macros, such as
+//   `declare_leaf_storage_kind`, are defined to be equivalent to others (i.e.
+//   `declare_storage_kind` in this case), when they are not defined. This
+//   allows us to use general definitions when sufficient (i.e. for any storage
+//   kind) and go in detail when needed.
+// - the second part is the hierarchy definition itself, see later for a
+//   description.
+// - the third part undefines all the macros after their use. This allows the
+//   including file to forget about them as soon as the #include <> directive is
+//   issued. Otherwise, the caller would be responsible to undef the defined
+//   macros before the next inclusion. 
+//
+// The hierarchy definition needs some terminology to be understood. This
+// terminology is similar to the one used in `core.hpp` and other places.
+// - a `hierarchy` is a top-level type which can be thought as the base class of
+//   all its subtypes. `formula`, `term`, etc... are hierarchies.
+// - a `storage kind` is a subtype of a given hierarchy that has a specific
+//   layout: a specific number of fields and children with their type. It's
+//   called in this way because the layout of storage kinds define how the
+//   `alphabet` class will store them in its internal structures. For example,
+//   the `unary` and `binary` types are storage kinds of the `formula`
+//   hierarchy. `unary` defines its layout to only have a single child called
+//   `argument`, while `binary` defines it to have two children called `left`
+//   and `right`. Storage kinds can, at run-time, be of different types. They
+//   always store their type (as a value of the `syntax_element` enum), so it is
+//   not listed here among the fields.
+// - a `hierarchy element` is the bottom level of the hierarchy, corresponding
+//   to a specific type of a storage kind. For example, `conjunction` and
+//   `disjunction` are two hierachy elements of the `binary` storage kind. They
+//   share all the fields and children from their parent storage kind, with the
+//   only difference being that the type is fixed and known at compile-time.
+// - a `field` is a data attribute of a storage kind (and of its hierarchy
+//   elements). For example, the `boolean` storage kind has the field `value`
+//   which is of type `bool`. This results into an actual member function called
+//   `value()` exposed by the class `boolean`.
+// - a `child` is a data attribute of a storage kind whose type is a hierarchy
+//   hierarchy and thus is subject to the same restrictions regarding its syntax
+//   as imposed by the fragment applied to the current storage kind. For
+//   example, `binary` has two children `left` and `right` of `formula`
+//   hierarchy. This results into two actual member functions called `left()`
+//   and `right()` exposed by the class `binary<Syntax>`, for some fragment
+//   `Syntax`, which return values of type `formula<Syntax>`.
+// - a `children vector` is a collection of a dynamic number of children of the
+//   same hierarchy. For example, the `atom` storage kind has a children vector
+//   `terms` of hierarchy `term`. This results into the class `atom<Syntax>`,
+//   for some fragment `Syntax`, to expose a member function called `terms()`
+//   which returns a range of elements of type `term<Syntax>`.
+// - a "leaf" entity is one that has no children and no hierarchy elements. As
+//   such, the corresponding type will be non-templated. For example, `boolean`
+//   is a leaf storage kind, since it only has a field `bool value` but no
+//   children and no hierarchy elements. The class `boolean` is concrete, not a
+//   template, since there are no children to constrain the fragment of.
+//   `zero` is a leaf hierarchy element of the storage kind `identity` 
+//   (hierarchy `number`). It has to be, because its parent storage kind
+//   defines no children.
+//
+// The macros used to define the entities listed above have a certain common
+// structure. Each macro, excepting for the top-level `declare_hierarchy`, has
+// to be put inside the pair `declare`/`end` of its parent macro, e.g.
+// `declare_storage_kind()` must appear inside a pair of
+// `declare_hierarchy()`/`end_hierarchy()`. Each invocation of a macro repeats
+// all the arguments of its parent macros. So for example, inside the call to
+// `declare_hierarchy(formula)`, we have the call to
+// `declare_storage_kind(formula, binary)`, and inside that we have
+// `declare_hierarchy_element(formula, binary, conjunction)`. This is a bit
+// repetitive but there is no other way to ensure that the expansion of each 
+// macro has all the context to know what to do. Some macros are tought to be 
+// used in pairs, e.g. `declare_hierarchy`/`end_hierarchy`. In this case, the 
+// `end_` macro takes always the same arguments as the `declare_` one.
+//
+// The actual macros are the following:
+//
+// - declare_hierarchy(Base)/end_hierarchy 
+//     Declare a hierarchy. 
+//     - Arguments:
+//       - `Base` is the name of the hierarchy (e.g. `formula`).
+//
+// - declare_storage_kind(Base, Storage)/end_storage_kind 
+//     Declare a storage kind. 
+//     - Parent macro: `declare_hierarchy` 
+//     - Arguments:
+//       - `Base` is the name of the parent hierarchy
+//       - `Storage` is the name of the storage kind 
+//
+// - declare_leaf_storage_kind(Base, Storage)/end_leaf_storage_kind 
+//     Declare a leaf storage kind. 
+//     - Parent macro: `declare_hierarchy` 
+//     - Arguments:
+//       - `Base` is the name of the parent hierarchy
+//       - `Storage` is the name of the storage kind 
+//
+// - declare_storage_custom_members(Base, Storage, Struct) 
+//     Declare a custom type declaring custom members for the given storage
+//     kind. The given identifier must name a template class taking a single
+//     type template parameter. This template class will be used as a CRTP base
+//     class for `storage_base` in `core.hpp`, thus providing additional custom
+//     members for all storage types and hierarchy elements of that type. 
+//     - Parent macro: `declare_storage_kind` or `declare_leaf_storage_kind` 
+//     - Arguments:
+//       - `Base` is the name of the parent hierarchy
+//       - `Storage` is the name of the parent storage kind 
+//       - `Struct` is the name of the custom members class
+//
+// - declare_field(Base, Storage, Type, Field) 
+//     Declare a member field of a storage kind. 
+//     - Parent macro: `declare_storage_kind` or `declare_leaf_storage_kind` 
+//     - Arguments:
+//       - `Base` is the name of the parent hierarchy
+//       - `Storage` is the name of the parent storage kind 
+//       - `Type` is the type of the field
+//       - `Field` is the name of the field
+//
+// - declare_child(Base, Storage, Hierarchy, Child) 
+//     Declare a child of a storage kind. 
+//     - Parent macro: `declare_storage_kind`
+//     - Arguments:
+//       - `Base` is the name of the parent hierarchy
+//       - `Storage` is the name of the parent storage kind 
+//       - `Hierarchy` is the hierarchy to which the child belongs
+//       - `Child`  is the name of the resulting field
+//
+// - declare_children(Base, Storage, Hierarchy, Children) 
+//     Declare a children vector for a storage kind. It must appear at most
+//     once for a given storage kind. 
+//     - Parent macro: `declare_storage_kind`
+//     - Arguments:
+//       - `Base` is the name of the parent hierarchy
+//       - `Storage` is the name of the parent storage kind 
+//       - `Hierarchy` is the hierarchy to which the children belong
+//       - `Children` is the name of the resulting field
+//
+// - declare_hierarchy_element(Base, Storage, Element) 
+//     Declares a hierarchy element for a given storage kind. At least one of 
+//     this, `declare_leaf_hierarchy_element`, or `has_no_hierarchy_elements` 
+//     must appear in a given storage kind. 
+//     - Parent macro: `declare_storage_kind`
+//     - Arguments:
+//       - `Base` is the name of the parent hierarchy
+//       - `Storage` is the name of the parent storage kind 
+//       - `Element` is the name of the element
+//
+// - declare_leaf_hierarchy_element(Base, Storage, Element) 
+//     Declares a hierarchy element for a given storage kind. At least one of 
+//     this, `declare_hierarchy_element`, or `has_no_hierarchy_elements` must 
+//     appear in a given storage kind. If this macro appears in a storage kind, 
+//     the latter cannot declare children.
+//     - Parent macro: `declare_storage_kind`
+//     - Arguments:
+//       - `Base` is the name of the parent hierarchy
+//       - `Storage` is the name of the parent storage kind 
+//       - `Element` is the name of the element
+//
+// - has_no_hierarchy_elements(Base, Storage) 
+//     States that the enclosing storage kind has no hierarchy elements. At 
+//     least one of this, `declare_hierarchy_element`, or 
+//     `declare_leaf_hierarchy_element` must appear in a given (non-leaf) 
+//     storage kind. It must not appear together with 
+//     `declare_leaf_hierarchy_element` or `declare_hierarchy_element`. 
+//     - Parent macro: `declare_storage_kind`
+//     - Arguments:
+//       - `Base` is the name of the parent hierarchy
+//       - `Storage` is the name of the parent storage kind 
 //
 
 #ifndef declare_hierarchy
