53ad0b34ea4b7306ded1a00d96c5fef72a94d051
Nicola Gigante <nicola.gigante@gmail.com>
2022-09-21 21:01:25 +0200

    New benchmark results

--- benchmarks/scripts/ls4/pre.sh
+++ benchmarks/scripts/ls4/pre.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-sed -e "s/~/not/g" -e "s/!/not/g" -e "s/X/next/g" -e "s/F/sometime/g" \
-    -e "s/G/always/g" -e "s/U/until/g" < $1 | translate
+trproblem=$(mktemp)
+
+sed -e "s/True/(AAA | not AAA)/g" -e "s/False/(AAA \& not AAA)/g" -e "s/~/not/g" -e "s/G/always/g" -e "s/F/sometime/g" -e "s/X/next/g" -e "s/U/until/g" \
+    -e "s/=>/->/g"  < $1 > $trproblem
+
+echo . >> $trproblem
+
+cat $trproblem
+
