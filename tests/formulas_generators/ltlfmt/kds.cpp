ede6692aaf21306b085a3f8b06d291a3c907f693
Nicola Gigante <nicola.gigante@gmail.com>
2023-01-14 11:27:10 +0100

    Better experimental domain

--- tests/formulas_generators/ltlfmt/kds.cpp
+++ tests/formulas_generators/ltlfmt/kds.cpp
@@ -71,6 +71,22 @@ inline auto frame_axiom(scope &xi, relation rel) {
     forall(decls, implies(X(sigma.top()), iff(rel(decls), wX(rel(decls)))));
 }
 
+inline formula true_if(formula f, bool b) {
+  if(b)
+    return f.sigma()->top();
+  return f;
+}
+
+[[noreturn]]
+static void usage(const char *argv0) {
+  std::cerr << "usage: " << argv0 << " <S> <P> <N>\n";
+  std::cerr << " - <S>: either 1 (simple) or 2 (arithmetic)\n";
+  std::cerr << " - <P>: property to test, either 1 (safety) or 2 (guarantee)\n";
+  std::cerr << " - <N>: number of iterations\n";
+
+  exit(1);
+}
+
 int main(int argc, char **argv) {
 
   if(argc < 3) {
@@ -78,102 +94,101 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  int P = atoi(argv[1]);
-  int N = atoi(argv[2]);
+  int S = atoi(argv[1]);
+  int P = atoi(argv[2]);
+  int N = atoi(argv[3]);
+
+  if(S != 1 && S != 2) {
+    std::cerr << argv[0] << ": error: S must be 1 or 2\n";
+    usage(argv[0]);
+  }
 
   if(P != 1 && P != 2) {
-    std::cerr << "Property must be 1 or 2\n";
-    return 1;
+    std::cerr << argv[0] << ": error: P must be 1 or 2\n";
+    usage(argv[0]);
   }
 
   if(N <= 0) {
-    std::cerr << "N must be positive\n";
-    return 1;
+    std::cerr << argv[0] << ": error: N must be positive\n";
+    usage(argv[0]);
   }
 
-  std::cout << "Solving property " << P << " for N = " << N << "...\n";
+  std::cout << 
+    "Solving " << ((S == 1) ? "simple" : "arithmetic") <<
+    " property " << P << " for N = " << N << "...\n";
 
   alphabet sigma;
   scope xi{sigma};
 
   auto u1 = sigma.variable("u1");
   auto u2 = sigma.variable("u2");
-  auto s1 = sigma.variable("s1");
-  auto s2 = sigma.variable("s2");
+  auto g1 = sigma.variable("g1");
+  auto g2 = sigma.variable("g2");
 
   auto user = sigma.named_sort("user");
   auto state = sigma.named_sort("state");
-  auto score = sigma.integer_sort();
+  auto group = sigma.named_sort("group");
 
   auto applications = sigma.relation("applications");
   auto winners = sigma.relation("winners");
 
-  xi.declare(applications, {user, score});
-  xi.declare(winners, {user, score});
+  xi.declare(applications, {user, group});
+  xi.declare(winners, {user, group});
 
-  auto x_status = sigma.variable("x_status");
-  auto x_winner = sigma.variable("x_winner");
-  auto x_score = sigma.variable("x_score");
-  auto x_evaluations = sigma.variable("x_evaluations");
+  auto x_state = sigma.variable("x_state");
+  auto x_winners = sigma.variable("x_winners");
+  auto x_u_winners = sigma.variable("x_u_winners");
 
-  xi.declare(x_status, state);
-  xi.declare(x_winner, user);
-  xi.declare(x_score, score);
-  xi.declare(x_evaluations, sigma.integer_sort());
+  xi.declare(x_state, state);
+  xi.declare(x_winners, sigma.integer_sort());
+  xi.declare(x_u_winners, sigma.integer_sort());
 
   auto c_init = sigma.variable("c_init");
-  auto c_app_phase = sigma.variable("c_app_phase");
-  auto c_apps_rcvd = sigma.variable("c_apps_rcvd");
-  auto c_evaluated = sigma.variable("c_evaluated");
+  auto c_app = sigma.variable("c_app");
+  auto c_eval = sigma.variable("c_eval");
   auto c_final = sigma.variable("c_final");
 
-  xi.declare(state, make_domain({
-    c_init, c_app_phase, c_apps_rcvd, c_evaluated, c_final
-  }));
-  
-  // auto c_undef_user = sigma.variable("c_undef_user");
-  // xi.declare(c_undef_user, user, scope::rigid);
+  xi.declare(state, make_domain({c_init, c_app, c_eval, c_final}));
+
+  auto c_majority = sigma.variable("c_majority");
+  auto c_underrepresented = sigma.variable("c_underrepresented");
+  xi.declare(group, make_domain({c_majority, c_underrepresented}));
 
   formula winners_primary_key = 
-    forall({u1[user], s1[score], s2[score]}, 
-      implies(winners(u1, s1) && winners(u1, s2), s1 == s2)
+    forall({u1[user], g1[group], g2[group]}, 
+      implies(winners(u1, g1) && winners(u1, g2), g1 == g2)
     );
   
   formula applications_primary_key = 
-    forall({u1[user], s1[score], s2[score]}, 
-      implies(applications(u1, s1) && applications(u1, s2), s1 == s2)
+    forall({u1[user], g1[group], g2[group]}, 
+      implies(applications(u1, g1) && applications(u1, g2), g1 == g2)
     );
 
   formula axioms = winners_primary_key && applications_primary_key;
 
-  formula reset =
-    x_status == c_init &&
-    forall({u1[user], s1[score]}, !applications(u1, s1));
-
-  formula init = reset && x_evaluations == N - 1 && 
-    x_score == 0 && //x_winner == c_undef_user &&
-    forall({u1[user], s1[score]}, !winners(u1, s1));
+  formula init = x_state == c_init && x_winners == 0 && x_u_winners == 0 &&
+    forall({u1[user], g1[group]}, !applications(u1, g1)) && 
+    forall({u1[user], g1[group]}, !winners(u1, g1));
 
   formula phi_tr = sigma.bottom();
 
+  // from `init` to `app` or the self-loop over `app`
   phi_tr = phi_tr || (
-    exists({u1[user], s1[score]}, 
+    exists({u1[user], g1[group]}, 
       // guard
-      (x_status == c_init || x_status == c_app_phase) && 
-      s1 >= 1 && s1 <= 100 && 
-      !applications(u1, s1) &&
+      (x_state == c_init || x_state == c_app) && 
+      !applications(u1, g1) &&
       // update
-      wnext(x_status) == c_app_phase &&
-      wnext(x_evaluations) == x_evaluations &&
-      wnext(x_score) == x_score &&
-      wnext(x_winner) == x_winner &&
-      wX(applications(u1, s1)) &&
+      wnext(x_state) == c_app &&
+      wX(applications(u1, g1)) &&
       // frames
-      forall({u2[user], s2[score]},
+      wnext(x_winners) == x_winners &&
+      wnext(x_u_winners) == x_u_winners &&
+      forall({u2[user], g2[group]},
         (
-          implies(u2 != u1 && s2 != s1, 
+          implies(u2 != u1 && g2 != g1, 
             implies(X(sigma.top()), 
-              iff(applications(u2, s2), wX(applications(u2, s2)))
+              iff(applications(u2, g2), wX(applications(u2, g2)))
             )
           )
         )
@@ -181,70 +196,61 @@ int main(int argc, char **argv) {
     )
   );
 
+  // from `app` to `eval`
   phi_tr = phi_tr || (
     // guard
-    x_status == c_app_phase &&
+    x_state == c_app &&
     // update
-    wnext(x_evaluations) == x_evaluations && 
-    wnext(x_status) == c_apps_rcvd &&
-    wnext(x_score) == x_score &&
-    wnext(x_winner) == x_winner &&
+    wnext(x_state) == c_eval &&
     // frames
+    wnext(x_winners) == x_winners &&
+    wnext(x_u_winners) == x_u_winners &&
     frame_axiom(xi, applications) &&
     frame_axiom(xi, winners)
   );
 
+  // self-loop over `eval`
   phi_tr = phi_tr || (
-    exists({u1[user], s1[score]}, 
+    exists({u1[user], g1[group]}, 
       // guard
-      x_status == c_apps_rcvd &&
-      applications(u1, s1) &&
-      //s1 > 80 &&
+      x_state == c_eval &&
+      applications(u1, g1) &&
       // update
-      wnext(x_evaluations) == x_evaluations &&
-      wnext(x_status) == c_evaluated &&
-      wnext(x_score) == x_score &&
-      wnext(x_winner) == x_winner &&
-      wX(winners(u1, s1)) &&
+      wnext(x_state) == c_eval &&
+      wnext(x_winners) == x_winners + 1 &&
+      implies(
+        g1 == c_underrepresented, wnext(x_u_winners) == x_u_winners + 1
+      ) &&
+      implies(g1 == c_majority, wnext(x_u_winners) == x_u_winners) &&
+      wX(winners(u1, g1)) &&
       // frames
-      forall({u2[user], s2[score]},
-        implies(u2 != u1 && s2 != s1, 
+      forall({u2[user], g2[group]},
+        implies(u2 != u1 && g2 != g1, 
           implies(X(sigma.top()), 
-            iff(winners(u2, s2), wX(winners(u2, s2)))
+            iff(winners(u2, g2), wX(winners(u2, g2)))
           )
         )
       )
     ) && frame_axiom(xi, applications)
   );
 
+  // from `eval` to `done`
   phi_tr = phi_tr || (
     // guard
-    x_status == c_evaluated && x_evaluations > 0 &&
+    x_state == c_eval && 
+    x_winners > N &&
+    //3 * x_u_winners > x_winners && // UNCOMMENT THIS to make it correct
     // update
-    wX(reset) &&
-    wnext(x_score) == x_score &&
-    wnext(x_winner) == x_winner &&
-    wnext(x_evaluations) == x_evaluations - 1 &&
+    wnext(x_state) == c_final &&
     // frames
-    frame_axiom(xi, winners)
-  );
-
-  phi_tr = phi_tr || (
-    exists({u1[user], s1[score]},
-      // guard
-      x_status == c_evaluated && x_evaluations == 0 && 
-      winners(u1, s1) &&
-      // update
-      wnext(x_status) == c_final &&
-      wnext(x_winner) == u1 &&
-      wnext(x_score) == s1
-    ) && 
-    // frames
-    frame_axiom(xi, winners)
+    wnext(x_winners) == x_winners &&
+    wnext(x_u_winners) == x_u_winners &&
+    frame_axiom(xi, winners) &&
+    frame_axiom(xi, applications)
   );
 
   [[maybe_unused]]
-  formula final = x_status == c_final;
+  formula final = x_state == c_final;
 
   formula system = 
     init && G(axioms) && G(implies(X(sigma.top()), phi_tr)) && F(final);
@@ -253,14 +259,40 @@ int main(int argc, char **argv) {
 
   formula property = sigma.top();
 
-  if(P == 1)
-    property =
-      G(implies(x_status == c_final, x_score > 60));
+  if(S == 1) {
+    if(P == 1)
+      property = G(
+        implies(
+          x_state == c_final, 
+          exists({u1[user], g1[group]}, 
+            winners(u1, g1) && g1 == c_underrepresented)
+        )
+      );
+    
+    if(P == 2)
+      property = G(
+        implies(
+          x_state == c_app, 
+          F(
+            x_state == c_final &&
+            exists({u1[user], g1[group]}, 
+              winners(u1, g1) && g1 == c_underrepresented)
+          )
+        )
+      );
+  } 
 
-  if(P == 2)
-    property = G(
-      implies(x_status == c_app_phase, F(x_status == c_final && x_score > 60))
-    );
+  if(S == 2) {
+    if(P == 1)
+      property =
+        G(implies(x_state == c_final, 3 * x_u_winners > x_winners));
+
+    if(P == 2)
+      property = G(
+        implies(x_state == c_app, F(x_state == c_final && x_u_winners > 1))
+      );
+  }
+  
 
   formula to_check = system && !property;
 
