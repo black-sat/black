525f9057e55fa502464554674311ccb7b7041a13
Nicola Gigante <nicola.gigante@gmail.com>
2023-01-14 14:52:37 +0100

    Final version of KDS experiment code

--- tests/formulas_generators/ltlfmt/kds.cpp
+++ tests/formulas_generators/ltlfmt/kds.cpp
@@ -26,37 +26,58 @@
 #include <black/solver/solver.hpp>
 
 #include <iostream>
+#include <functional>
 #include <string_view>
 
 using namespace black;
 
-[[maybe_unused]]
-static void tracer(black::solver::trace_t trace) {
-  static size_t k = 0;
-  using black::logic::fragments::FO::formula;
+//
+// Representation of KDSs, their encoding into LTLfMT, and their verification
+//
+struct KDS {
+  scope xi;
+  formula init;
+  formula axioms;
+  std::vector<formula> trans;
+  formula final;
+};
 
-  if(trace.type == black::solver::trace_t::stage) {
-    k = std::get<size_t>(trace.data);
-    std::cerr << "k: " << k << "\n";
-  }
-
-  // if(trace.type == black::solver::trace_t::unrav) {
-  //   std::cerr << k << "-unrav: " << 
-  //     to_string(std::get<formula>(trace.data)) << "\n";
-  // }
-
-  // if(trace.type == black::solver::trace_t::empty) {
-  //   std::cerr << k << "-empty: " << 
-  //     to_string(std::get<formula>(trace.data)) << "\n";
-  // }
+static tribool verify_kds(KDS kds, formula property) {
+  auto [xi, init, axioms, trans, final] = std::move(kds);
+  alphabet &sigma = *xi.sigma();
   
-  // if(trace.type == black::solver::trace_t::prune) {
-  //   std::cerr << k << "-prune: " << 
-  //     to_string(std::get<formula>(trace.data)) << "\n";
-  // }
+  formula trans_phi = big_or(sigma, trans);
+  formula system = 
+    init && G(axioms) && G(implies(X(sigma.top()), trans_phi)) && F(final);
+
+  formula to_check = system && !property;
+
+  [[maybe_unused]]
+  bool ok = xi.type_check(to_check, [&](auto s) {
+    std::cerr << "Type checking error: " << s << "\n";
+    std::cerr << "Formula: " << to_string(to_check) << "\n";
+  });
+  black_assert(ok);
+
+  solver slv;
+
+  slv.set_tracer([](black::solver::trace_t trace) {
+    if(trace.type == black::solver::trace_t::stage)
+      std::cerr << ".";
+  });
+
+  return slv.solve(
+    xi, to_check, 
+    /* finite = */ true, 
+    std::numeric_limits<size_t>::max(),
+    /* semi_decidable = */ true
+  );
 }
 
-inline auto frame_axiom(scope &xi, relation rel) {
+//
+// Modeling of the two example KDSs
+//
+static auto frame_axiom(scope &xi, relation rel) {
   using namespace std::literals;
 
   alphabet &sigma = *xi.sigma();
@@ -71,53 +92,14 @@ inline auto frame_axiom(scope &xi, relation rel) {
     forall(decls, implies(X(sigma.top()), iff(rel(decls), wX(rel(decls)))));
 }
 
-inline formula true_if(formula f, bool b) {
-  if(b)
+static formula true_unless(formula f, bool b) {
+  if(!b)
     return f.sigma()->top();
   return f;
 }
 
-[[noreturn]]
-static void usage(const char *argv0) {
-  std::cerr << "usage: " << argv0 << " <S> <P> <N>\n";
-  std::cerr << " - <S>: either 1 (simple) or 2 (arithmetic)\n";
-  std::cerr << " - <P>: property to test, either 1 (safety) or 2 (guarantee)\n";
-  std::cerr << " - <N>: number of iterations\n";
-
-  exit(1);
-}
-
-int main(int argc, char **argv) {
-
-  if(argc < 3) {
-    std::cerr << "Please specify number of phases\n";
-    return 1;
-  }
-
-  int S = atoi(argv[1]);
-  int P = atoi(argv[2]);
-  int N = atoi(argv[3]);
-
-  if(S != 1 && S != 2) {
-    std::cerr << argv[0] << ": error: S must be 1 or 2\n";
-    usage(argv[0]);
-  }
-
-  if(P != 1 && P != 2) {
-    std::cerr << argv[0] << ": error: P must be 1 or 2\n";
-    usage(argv[0]);
-  }
-
-  if(N <= 0) {
-    std::cerr << argv[0] << ": error: N must be positive\n";
-    usage(argv[0]);
-  }
-
-  std::cout << 
-    "Solving " << ((S == 1) ? "simple" : "arithmetic") <<
-    " property " << P << " for N = " << N << "...\n";
-
-  alphabet sigma;
+static KDS model_kds(alphabet &sigma, bool simple, size_t N) {
+  using namespace std::literals;
   scope xi{sigma};
 
   auto u1 = sigma.variable("u1");
@@ -137,22 +119,34 @@ int main(int argc, char **argv) {
 
   auto x_state = sigma.variable("x_state");
   auto x_winners = sigma.variable("x_winners");
-  auto x_u_winners = sigma.variable("x_u_winners");
+  auto x_under = sigma.variable("x_under");
 
   xi.declare(x_state, state);
-  xi.declare(x_winners, sigma.integer_sort());
-  xi.declare(x_u_winners, sigma.integer_sort());
+
+  if(!simple) {
+    xi.declare(x_winners, sigma.integer_sort());
+    xi.declare(x_under, sigma.integer_sort());
+  }
 
   auto c_init = sigma.variable("c_init");
   auto c_app = sigma.variable("c_app");
   auto c_eval = sigma.variable("c_eval");
   auto c_final = sigma.variable("c_final");
 
-  xi.declare(state, make_domain({c_init, c_app, c_eval, c_final}));
+  std::vector<variable> eval_n;
+  for(size_t i = 0; i < N; i++)
+    eval_n.push_back(sigma.variable(std::tuple{"c_eval"sv, i}));
+
+  std::vector<variable> states = {c_init, c_app, c_eval, c_final};
+
+  if(simple)
+    states.insert(states.end(), eval_n.begin(), eval_n.end());
+
+  xi.declare(state, make_domain(states));
 
   auto c_majority = sigma.variable("c_majority");
-  auto c_underrepresented = sigma.variable("c_underrepresented");
-  xi.declare(group, make_domain({c_majority, c_underrepresented}));
+  auto c_under = sigma.variable("c_under");
+  xi.declare(group, make_domain({c_majority, c_under}));
 
   formula winners_primary_key = 
     forall({u1[user], g1[group], g2[group]}, 
@@ -166,14 +160,15 @@ int main(int argc, char **argv) {
 
   formula axioms = winners_primary_key && applications_primary_key;
 
-  formula init = x_state == c_init && x_winners == 0 && x_u_winners == 0 &&
+  formula init = x_state == c_init && 
+    true_unless(x_winners == 0 && x_under == 0, !simple) &&
     forall({u1[user], g1[group]}, !applications(u1, g1)) && 
     forall({u1[user], g1[group]}, !winners(u1, g1));
 
-  formula phi_tr = sigma.bottom();
+  std::vector<formula> trans;
 
   // from `init` to `app` or the self-loop over `app`
-  phi_tr = phi_tr || (
+  trans.push_back(
     exists({u1[user], g1[group]}, 
       // guard
       (x_state == c_init || x_state == c_app) && 
@@ -182,8 +177,8 @@ int main(int argc, char **argv) {
       wnext(x_state) == c_app &&
       wX(applications(u1, g1)) &&
       // frames
-      wnext(x_winners) == x_winners &&
-      wnext(x_u_winners) == x_u_winners &&
+      true_unless(wnext(x_winners) == x_winners, !simple) &&
+      true_unless(wnext(x_under) == x_under, !simple) &&
       forall({u2[user], g2[group]},
         (
           implies(u2 != u1 && g2 != g1, 
@@ -197,31 +192,59 @@ int main(int argc, char **argv) {
   );
 
   // from `app` to `eval`
-  phi_tr = phi_tr || (
+  trans.push_back(
     // guard
     x_state == c_app &&
     // update
-    wnext(x_state) == c_eval &&
+    true_unless(wnext(x_state) == c_eval, !simple) &&
+    true_unless(wnext(x_state) == eval_n[0], simple) &&
     // frames
-    wnext(x_winners) == x_winners &&
-    wnext(x_u_winners) == x_u_winners &&
+    true_unless(wnext(x_winners) == x_winners, !simple) &&
+    true_unless(wnext(x_under) == x_under, !simple) &&
     frame_axiom(xi, applications) &&
     frame_axiom(xi, winners)
   );
 
+  // from `eval_i` to `eval_{i+1}` and `eval_{N-1}` to `c_eval`
+  if(simple) {
+    for(size_t i = 0; i < N; ++i) {
+      trans.push_back(
+        exists({u1[user], g1[group]}, 
+          // guard
+          x_state == eval_n[i] &&
+          applications(u1, g1) &&
+          // update
+          wnext(x_state) == (i == N - 1 ? c_eval : eval_n[i + 1]) &&
+          wX(winners(u1, g1)) &&
+          // frames
+          forall({u2[user], g2[group]},
+            implies(u2 != u1 && g2 != g1, 
+              implies(X(sigma.top()), 
+                iff(winners(u2, g2), wX(winners(u2, g2)))
+              )
+            )
+          )
+        ) && frame_axiom(xi, applications)
+      );
+    }
+
+  }
+
   // self-loop over `eval`
-  phi_tr = phi_tr || (
+  trans.push_back(
     exists({u1[user], g1[group]}, 
       // guard
       x_state == c_eval &&
       applications(u1, g1) &&
       // update
       wnext(x_state) == c_eval &&
-      wnext(x_winners) == x_winners + 1 &&
-      implies(
-        g1 == c_underrepresented, wnext(x_u_winners) == x_u_winners + 1
+      true_unless(wnext(x_winners) == x_winners + 1, !simple) &&
+      true_unless(
+        implies(g1 == c_under, wnext(x_under) == x_under + 1), !simple
       ) &&
-      implies(g1 == c_majority, wnext(x_u_winners) == x_u_winners) &&
+      true_unless(
+        implies(g1 == c_majority, wnext(x_under) == x_under), !simple
+      ) &&
       wX(winners(u1, g1)) &&
       // frames
       forall({u2[user], g2[group]},
@@ -235,97 +258,137 @@ int main(int argc, char **argv) {
   );
 
   // from `eval` to `done`
-  phi_tr = phi_tr || (
+  trans.push_back(
     // guard
     x_state == c_eval && 
-    x_winners > N &&
-    //3 * x_u_winners > x_winners && // UNCOMMENT THIS to make it correct
+    true_unless(x_winners > N, !simple) &&
+    true_unless(3 * x_under > x_winners, !simple) &&
+    true_unless(exists({u1[user]}, winners(u1, c_under)), simple) &&
     // update
     wnext(x_state) == c_final &&
     // frames
-    wnext(x_winners) == x_winners &&
-    wnext(x_u_winners) == x_u_winners &&
+    true_unless(wnext(x_winners) == x_winners, !simple) &&
+    true_unless(wnext(x_under) == x_under, !simple) &&
     frame_axiom(xi, winners) &&
     frame_axiom(xi, applications)
   );
 
-  [[maybe_unused]]
   formula final = x_state == c_final;
 
-  formula system = 
-    init && G(axioms) && G(implies(X(sigma.top()), phi_tr)) && F(final);
+  return KDS{std::move(xi), init, axioms, trans, final};
+}
 
-  // -------
+//
+// Properties to test
+//
+static formula mk_property(alphabet &sigma, int S, int P) {
+  auto u1 = sigma.variable("u1");
+  auto u2 = sigma.variable("u2");
+  auto x_state = sigma.variable("x_state");
+  auto x_under = sigma.variable("x_under");
+  auto x_winners = sigma.variable("x_winners");
+  auto c_under = sigma.variable("c_under");
+  auto c_app = sigma.variable("c_app");
+  auto c_final = sigma.variable("c_final");
 
-  formula property = sigma.top();
+  auto user = sigma.named_sort("user");
+  
+  auto winners = sigma.relation("winners");
 
   if(S == 1) {
+    formula gamma = 2 * x_under > x_winners;
+
     if(P == 1)
-      property = G(
-        implies(
-          x_state == c_final, 
-          exists({u1[user], g1[group]}, 
-            winners(u1, g1) && g1 == c_underrepresented)
-        )
-      );
-    
+      return G(implies(x_state == c_final, gamma));
+
     if(P == 2)
-      property = G(
-        implies(
-          x_state == c_app, 
-          F(
-            x_state == c_final &&
-            exists({u1[user], g1[group]}, 
-              winners(u1, g1) && g1 == c_underrepresented)
-          )
-        )
-      );
-  } 
+      return G(implies(x_state == c_app, F(x_state == c_final && gamma)));
+  }
 
   if(S == 2) {
-    if(P == 1)
-      property =
-        G(implies(x_state == c_final, 3 * x_u_winners > x_winners));
-
-    if(P == 2)
-      property = G(
-        implies(x_state == c_app, F(x_state == c_final && x_u_winners > 1))
+    formula gamma = // exists({u1[user]}, winners(u1, c_under));
+      exists({u1[user], u2[user]}, 
+        u1 != u2 && winners(u1, c_under) && winners(u2, c_under)
       );
+
+    if(P == 1)
+      return G(implies(x_state == c_final, gamma));
+    
+    if(P == 2)
+      return G(implies(x_state == c_app, F(x_state == c_final && gamma)));
+  } 
+
+  black_unreachable();
+}
+
+//
+// Command line handling and set up of the experiment
+//
+
+[[noreturn]]
+static void usage(const char *argv0) {
+  std::cerr << "usage: " << argv0 << " <S> <P> <N>\n";
+  std::cerr << " - <S>: either 1 (arithmetic) or 2 (simple)\n";
+  std::cerr << 
+    " - <P>: property to test, either `s` (safety) or `l` (liveness)\n";
+  std::cerr << " - <N>: number of iterations\n";
+
+  exit(1);
+}
+
+int main(int argc, char **argv) 
+{
+  using namespace std::literals;
+
+  if(argc < 4)
+    usage(argv[0]);
+
+  int S = atoi(argv[1]);
+  int A3 = atoi(argv[3]);
+
+  if(S != 1 && S != 2) {
+    std::cerr << argv[0] << ": error: S must be 1 or 2\n";
+    usage(argv[0]);
   }
+
+  int P = 0;
+  if(argv[2] == "s"sv)
+    P = 1;
+  if(argv[2] == "l"sv)
+    P = 2;
+
+  if(P != 1 && P != 2) {
+    std::cerr << argv[0] << ": error: P must be `s` or `l`\n";
+    usage(argv[0]);
+  }
+
+  if(A3 <= 0) {
+    std::cerr << argv[0] << ": error: N must be positive\n";
+    usage(argv[0]);
+  }
+
+  size_t N = size_t(A3);
+
+  std::cout << 
+    "Verifying " << ((S == 2) ? "simple" : "arithmetic") <<
+    " property " << P << " for N = " << N << "...";
+
+  bool simple = S == 2;
+
+  alphabet sigma;
+
+  KDS system = model_kds(sigma, simple, N);
+  formula property = mk_property(sigma, S, P);
   
+  tribool result = verify_kds(std::move(system), property);
 
-  formula to_check = system && !property;
-
-  bool ok = xi.type_check(to_check, [](auto s) {
-    std::cerr << "Type checking error: " << s << "\n";
-    exit(1);
-  });
-  black_assert(ok);
-
-  solver slv;
-
-  slv.set_tracer(tracer);
-  //slv.set_sat_backend("cvc5");
-
-  tribool result = slv.solve(
-    xi, to_check, 
-    /* finite = */true, 
-    std::numeric_limits<size_t>::max(),
-    /* semi_decidable = */true);
-
-  if(result == true)
-    std::cout << "SAT\n";
-  else if(result == false)
-    std::cout << "UNSAT\n";
+  if(result == true) {
+    std::cout << " Bug found!\n";
+    return 0;
+  } else if(result == false)
+    std::cout << " System verified!\n";
   else
-    std::cout << "UNKNOWN\n";
+    std::cout << " UNKNOWN\n";
 
-  // if(result == true) {
-  //   auto m = slv.model();
-  //   black_assert(m);
-
-  //   std::cout << m->dump();
-  // }
-
-  return 0;
+  return 1;
 }
