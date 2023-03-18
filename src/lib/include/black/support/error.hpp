dd0eda16af08ce66e9dad9908cc5d48b795e3e23
Nicola Gigante <nicola.gigante@gmail.com>
2023-03-18 23:31:38 +0100

    Initial code for user errors taxonomy

---
+++ src/lib/include/black/support/error.hpp
@@ -0,0 +1,58 @@
+//
+// BLACK - Bounded Ltl sAtisfiability ChecKer
+//
+// (C) 2019 Nicola Gigante
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef BLACK_SUPPORT_ERROR_HPP
+#define BLACK_SUPPORT_ERROR_HPP
+
+#include <fmt/format.h>
+#include <system_error>
+
+namespace black::support {
+
+  struct user_error {
+    const char *msg = nullptr;
+    fmt::format_args msg_args;
+  }
+
+  struct syntax_error : user_error {
+    std::optional<std::string> filename;
+    size_t line = 0;
+    size_t column = 0;
+    const char *msg = nullptr;
+    fmt::format_args msg_args;
+  };
+
+  struct type_error : user_error { };
+
+  struct backend_error : user_error { };
+
+  struct io_error : user_error {
+    std::optional<std::string> filename;
+    std::error_code error;
+  };
+
+  struct error = black_union_type(syntax_error, io_error);
+
+}
+
+#endif // BLACK_SUPPORT_ERROR_HPP
