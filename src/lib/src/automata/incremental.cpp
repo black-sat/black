f38a8d41bd0bce8ea73726b7dddfcf0bf520b9a4
Nicola Gigante <nicola.gigante@gmail.com>
2023-04-19 20:44:01 +0200

    Optimizations

--- src/lib/src/automata/incremental.cpp
+++ src/lib/src/automata/incremental.cpp
@@ -79,12 +79,11 @@ namespace black_internal {
     automaton product(automaton, automaton);
     automaton sum(automaton, automaton);
     automaton negation(automaton);
-    automaton not_empty();
+    automaton not_empty(automaton);
 
     automaton encode(formula f);
 
-    automaton to_automaton(logic::boolean, bool value);
-    automaton to_automaton(logic::proposition, black::identifier name);
+    automaton to_automaton(logic::formula<logic::propositional> f);
     automaton to_automaton(logic::negation<LTLXFG>, formula arg);
     automaton to_automaton(logic::conjunction<LTLXFG>, formula, formula);
     automaton to_automaton(logic::disjunction<LTLXFG>, formula, formula);
@@ -96,10 +95,15 @@ namespace black_internal {
     automaton to_automaton(logic::w_tomorrow<LTLXFG>, formula arg);
     automaton to_automaton(formula f);
     
+    automaton to_automaton(logic::otherwise, auto ...) {
+      black_unreachable();
+    }
+
     sdd::manager *mgr;
     logic::alphabet &sigma;
     std::vector<logic::proposition> letters;
     size_t next_fresh = 0;
+    size_t indentn = 0;
   };
 
   [[maybe_unused]]
@@ -194,6 +198,8 @@ namespace black_internal {
   }
 
   sdd::variable incremental_t::fresh(black::identifier id) {
+    std::string indent(indentn * 3, ' ');
+    std::cerr << indent << "allocating fresh variable\n";
     return mgr->variable(
       sigma.proposition(
         fresh::fresh_t{sigma.proposition(id), next_fresh++}
@@ -202,18 +208,13 @@ namespace black_internal {
   }
 
   automaton incremental_t::negation(automaton arg) {
-    sdd::variable var = fresh();
-
-    std::vector<logic::proposition> variables = arg.variables;
-    variables.push_back(var.name());
-
     return automaton{
       .manager = mgr,
       .letters = letters,
-      .variables = variables,
-      .init = var && arg.init,
-      .trans = arg.trans && !prime(var),
-      .finals = !arg.finals && !var
+      .variables = arg.variables,
+      .init = arg.init,
+      .trans = arg.trans,
+      .finals = !arg.finals
     };
   }
 
@@ -221,12 +222,23 @@ namespace black_internal {
     std::vector<logic::proposition> variables = a1.variables;
     variables.insert(end(variables), begin(a2.variables), end(a2.variables));
 
+    std::string indent(indentn * 3, ' ');
+
+    std::cerr << indent << "computing trans1 && trans2...\n";
+    sdd::node trans = a1.trans && a2.trans;
+    std::cerr << indent << " - size: " << trans.count() << "\n";
+    std::cerr << indent << " - minimizing...\n";
+
+    trans.minimize();
+
+    std::cerr << indent << " - minimized: " << trans.count() << "\n";
+
     return automaton {
       .manager = mgr,
       .letters = letters,
       .variables = variables,
       .init = a1.init && a2.init,
-      .trans = (a1.trans && a2.trans).minimize(),
+      .trans = trans,
       .finals = a1.finals && a2.finals
     };
   }
@@ -235,29 +247,37 @@ namespace black_internal {
     return negation(product(negation(a1), negation(a2)));
   }
 
+  automaton incremental_t::not_empty(automaton aut) {
+    sdd::variable var = fresh();
+
+    std::vector<logic::proposition> variables = aut.variables;
+    variables.push_back(var.name());
+
+    return automaton{
+      .manager = mgr,
+      .letters = letters,
+      .variables = variables,
+      .init = var && aut.init,
+      .trans = aut.trans && !prime(var),
+      .finals = aut.finals && !var
+    };
+  }
+
   automaton incremental_t::encode(formula f) {
     collect_letters(f);
-    return to_automaton(f);
+    return not_empty(to_automaton(f));
   }
 
   automaton incremental_t::to_automaton(formula f) {
+    if(auto b = f.to<logic::formula<logic::propositional>>(); b)
+      return to_automaton(*b);
+
     return f.match(
       [&](auto ...args) { return to_automaton(args...); }
     );
   }
 
-  automaton incremental_t::to_automaton(logic::boolean, bool value) {
-    return automaton{
-      .manager = mgr,
-      .letters = letters,
-      .variables = {},
-      .init = mgr->top(),
-      .trans = mgr->top(),
-      .finals = value ? mgr->top() : mgr->bottom()
-    };
-  }
-
-  automaton incremental_t::to_automaton(logic::proposition p, black::identifier)
+  automaton incremental_t::to_automaton(logic::formula<logic::propositional> f)
   {
     sdd::variable var = fresh();
 
@@ -266,7 +286,7 @@ namespace black_internal {
       .letters = letters,
       .variables = {var.name()},
       .init = !var,
-      .trans = iff(prime(var), var || mgr->variable(p)),
+      .trans = iff(prime(var), var || mgr->to_node(f)),
       .finals = var
     };
   }
@@ -278,19 +298,40 @@ namespace black_internal {
   automaton incremental_t::to_automaton(
     logic::conjunction<LTLXFG> conj, formula, formula
   ) {
-    automaton aut = to_automaton(sigma.top());
-    for(auto op : conj.operands())
-      aut = product(aut, to_automaton(op));
+    using namespace std;
+
+    std::string indent(indentn * 3, ' ');
+
+    std::vector<formula> operands(begin(conj.operands()), end(conj.operands()));
+    auto temp = std::partition(begin(operands), end(operands), [](formula op) {
+      return op.is<logic::formula<logic::propositional>>();
+    });
+
+    std::cerr << indent << "found " << std::distance(begin(operands), temp)
+              << " boolean subformulas in conjunction\n";
+
+    formula booleanf = 
+      big_and(sigma, std::ranges::subrange(begin(operands), temp));
+
+    std::cerr << indent << "building the boolean part...\n";
+    automaton aut = to_automaton(booleanf);
+    std::cerr << indent << "done!\n";
+    for(auto it = temp; it != end(operands); ++it) {
+      std::cerr << indent << "building a product ...\n";
+      indentn++;
+      aut = product(aut, to_automaton(*it));
+      indentn--;
+      std::cerr << indent << "done!\n";
+    }
     return aut;
   }
 
   automaton incremental_t::to_automaton(
     logic::disjunction<LTLXFG> disj, formula, formula
   ) {
-    automaton aut = to_automaton(sigma.top());
-    for(auto op : disj.operands())
-      aut = product(aut, negation(to_automaton(op)));
-    return negation(aut);
+    return to_automaton(!big_and(sigma, disj.operands(), [](auto op) {
+      return !op;
+    }));
   }
 
   automaton incremental_t::to_automaton(
@@ -327,7 +368,14 @@ namespace black_internal {
       .finals = !var && aut.finals
     };
 
-    return sum(aut,  cover(semideterminize(xf))); // aut || xf
+    std::string indent(indentn * 3, ' ');
+
+    std::cerr << indent << "semideterminizing...\n";
+    std::cerr << indent << " - vars: " << xf.variables.size() << "\n";
+    std::cerr << indent << " - size: " << xf.trans.count() << "\n";
+    automaton semixf = semideterminize(xf);
+
+    return sum(aut, cover(semixf)); // aut || xf
   }
   
   automaton incremental_t::to_automaton(logic::always<LTLXFG>, formula arg) {
