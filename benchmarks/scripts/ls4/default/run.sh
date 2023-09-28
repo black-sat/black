ec284f78d457c52d9615872137ade0a0a8618921
Nicola Gigante <nicola.gigante@gmail.com>
2023-09-28 10:10:55 +0200

    New ls4 run script and finite-traces results

--- benchmarks/scripts/ls4/default/run.sh
+++ benchmarks/scripts/ls4/default/run.sh
@@ -2,9 +2,5 @@
 
 set -e
 
-lsproblem=$(mktemp)
-translate=$(which TST-translate.pl)
 
-swipl --stack_limit=4G -s $translate -g go -t "halt(1)" -- $1 $lsproblem 
-
-ls4 < $lsproblem
+ltl2snf+ls4.sh $1
--- benchmarks/scripts/ls4/pre.sh
+++ benchmarks/scripts/ls4/pre.sh
@@ -2,10 +2,8 @@
 
 trproblem=$(mktemp)
 
-sed -e "s/True/(AAA | not AAA)/g" -e "s/False/(AAA \& not AAA)/g" -e "s/~/not/g" -e "s/G/always/g" -e "s/F/sometime/g" -e "s/X/next/g" -e "s/U/until/g" \
+sed -e "s/True/true/g" -e "s/False/false/g" -e "s/~/not/g" -e "s/G/always/g" -e "s/F/sometime/g" -e "s/X/next/g" -e "s/U/until/g" \
     -e "s/=>/->/g"  < $1 > $trproblem
-
-echo . >> $trproblem
 
 cat $trproblem
 
